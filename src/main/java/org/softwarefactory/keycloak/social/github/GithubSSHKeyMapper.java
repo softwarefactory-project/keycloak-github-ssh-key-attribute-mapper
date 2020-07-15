68a4c66ba3bdbf2c4d47234f5dbcee2577bfb376
Matthieu Huin <mhuin@redhat.com>
2020-07-15 12:14:19 +0200

    Make compatible with Keycloak 10.0.2
    Depends-On: https://softwarefactory-project.io/r/18836
    
    Change-Id: I6f91151db9a23474adc0e42aee89eb53dcb24ecd
    Co-Authored-By: Thomas Darimont <thomas.darimont@googlemail.com>

--- src/main/java/org/softwarefactory/keycloak/social/github/GithubSSHKeyMapper.java
+++ src/main/java/org/softwarefactory/keycloak/social/github/GithubSSHKeyMapper.java
@@ -18,99 +18,142 @@
 package org.softwarefactory.keycloak.social.github;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import org.jboss.logging.Logger;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.google.auto.service.AutoService;
+import lombok.extern.jbosslog.JBossLog;
 import org.keycloak.broker.provider.AbstractIdentityProviderMapper;
 import org.keycloak.broker.provider.BrokeredIdentityContext;
 import org.keycloak.broker.provider.IdentityBrokerException;
+import org.keycloak.broker.provider.IdentityProviderMapper;
 import org.keycloak.broker.provider.util.SimpleHttp;
 import org.keycloak.models.IdentityProviderMapperModel;
+import org.keycloak.models.IdentityProviderMapperSyncMode;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.provider.ProviderConfigProperty;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+@JBossLog
+@AutoService(IdentityProviderMapper.class)
 public class GithubSSHKeyMapper extends AbstractIdentityProviderMapper {
-  private static final String[] cp = new String[] { "github" };
 
-  protected static final Logger logger = Logger.getLogger(GithubSSHKeyMapper.class);
+    private static final String[] COMPATIBLE_PROVIDERS = {"github"};
 
-  protected static final Logger LOGGER_DUMP_USER_PROFILE = Logger.getLogger("org.keycloak.social.user_profile_dump");
+    public static final String SSH_KEYS_URL = "https://api.github.com/users/%s/keys";
 
-  public static final String SSH_URL = "https://api.github.com/users/%s/keys";
+    public static final String CONF_KEY_ATTRIBUTE = "keyAttribute";
 
-  public static final String CONF_KEY_ATTRIBUTE = "keyAttribute";
+    private static final List<ProviderConfigProperty> configProperties;
 
-  private static final List<ProviderConfigProperty> configProperties = new ArrayList<ProviderConfigProperty>();
-
-  static {
-    ProviderConfigProperty property = new ProviderConfigProperty();
-    property.setName("keyAttribute");
-    property.setLabel("User Attribute Name");
-    property.setHelpText("User attribute name to store the SSH public keys into.");
-    property.setType("String");
-    configProperties.add(property);
-  }
-
-  public String[] getCompatibleProviders() {
-    return cp;
-  }
-
-  public String getId() {
-    return "github-ssh-key-mapper";
-  }
-
-  public List<ProviderConfigProperty> getConfigProperties() {
-    return configProperties;
-  }
-
-  public String getDisplayCategory() {
-    return "SSH Keys Importer";
-  }
-
-  public String getDisplayType() {
-    return "SSH Keys Importer";
-  }
-
-  public String getHelpText() {
-    return "Import user SSH keys into the specified user attribute.";
-  }
-
-  public void updateBrokeredUser(KeycloakSession session, RealmModel realm, UserModel user, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {}
-
-  public void preprocessFederatedIdentity(KeycloakSession session, RealmModel realm, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
-    String attribute = (String)mapperModel.getConfig().get("keyAttribute");
-    if (attribute == null || attribute.trim().isEmpty()) {
-      logger.warnf("Attribute is not configured for mapper %s", mapperModel.getName());
-      return;
+    static {
+        ProviderConfigProperty keysAttributeProperty = new ProviderConfigProperty();
+        keysAttributeProperty.setName(CONF_KEY_ATTRIBUTE);
+        keysAttributeProperty.setLabel("User Attribute Name");
+        keysAttributeProperty.setHelpText("User attribute name to store the SSH public keys into.");
+        keysAttributeProperty.setType("String");
+        configProperties = Collections.singletonList(keysAttributeProperty);
     }
-    attribute = attribute.trim();
-    Object value = getKeys(context, session);
-    if (value != null)
-      if (value instanceof List) {
-        context.setUserAttribute(attribute, (List)value);
-      } else {
-        context.setUserAttribute(attribute, value.toString());
-      }
-  }
 
-  protected static Object getKeys(BrokeredIdentityContext context, KeycloakSession session) {
-    String username = context.getUsername();
-    String keys_url = String.format("https://api.github.com/users/%s/keys", new Object[] { username });
-    List<String> keys = new ArrayList<>();
-    try {
-      ObjectNode keys_list = (ObjectNode)SimpleHttp.doGet(keys_url, session).asJson();
-      Iterator<JsonNode> loop = keys_list.elements();
-      while (loop.hasNext()) {
-        JsonNode key = loop.next();
-        keys.add(key.get("key").textValue());
-      }
-      return keys;
-    } catch (Exception e) {
-      throw new IdentityBrokerException("Could not obtain user public keys from github.", e);
+    public String[] getCompatibleProviders() {
+        return COMPATIBLE_PROVIDERS;
     }
-  }
+
+    public String getId() {
+        return "github-ssh-key-mapper";
+    }
+
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+
+    public String getDisplayCategory() {
+        return "SSH Keys Importer";
+    }
+
+    public String getDisplayType() {
+        return "SSH Keys Importer";
+    }
+
+    public String getHelpText() {
+        return "Import user SSH keys into the specified user attribute.";
+    }
+
+    @SuppressWarnings("unchecked")
+    public void updateBrokeredUser(KeycloakSession session, RealmModel realm, UserModel user, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+
+        if (!IdentityProviderMapperSyncMode.FORCE.equals(mapperModel.getSyncMode())) {
+            return;
+        }
+
+        fetchKeysAndUpdateAttributes(session, mapperModel, context, (keyAttribute, keys) -> {
+            if (keys instanceof List) {
+                user.setAttribute(keyAttribute, (List<String>) keys);
+            } else {
+                user.setSingleAttribute(keyAttribute, keys.toString());
+            }
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    public void preprocessFederatedIdentity(KeycloakSession session, RealmModel realm, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+
+        fetchKeysAndUpdateAttributes(session, mapperModel, context, (keyAttribute, keys) -> {
+
+            if (keys instanceof List) {
+                context.setUserAttribute(keyAttribute, (List<String>) keys);
+            } else {
+                context.setUserAttribute(keyAttribute, keys.toString());
+            }
+        });
+    }
+
+    private void fetchKeysAndUpdateAttributes(KeycloakSession session, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context, BiConsumer<String, Object> updater) {
+
+        String attribute = getKeyAttribute(mapperModel);
+        if (attribute == null) {
+            return;
+        }
+
+        Object value = getKeys(context, session);
+
+        if (value == null) {
+            return;
+        }
+
+        updater.accept(attribute, value);
+    }
+
+    private String getKeyAttribute(IdentityProviderMapperModel mapperModel) {
+
+        String attribute = mapperModel.getConfig() == null ? null : mapperModel.getConfig().get(CONF_KEY_ATTRIBUTE);
+        if (attribute == null || attribute.trim().isEmpty()) {
+            log.warnf("Attribute is not configured for mapper %s", mapperModel.getName());
+            return null;
+        }
+        return attribute.trim();
+    }
+
+    protected static Object getKeys(BrokeredIdentityContext context, KeycloakSession session) {
+
+        String keysUrl = String.format(SSH_KEYS_URL, context.getUsername());
+
+        List<String> keys = new ArrayList<>();
+        try {
+            ArrayNode keyList = (ArrayNode) SimpleHttp.doGet(keysUrl, session).asJson();
+            Iterator<JsonNode> loop = keyList.elements();
+            while (loop.hasNext()) {
+                JsonNode key = loop.next();
+                keys.add(key.get("key").textValue());
+            }
+            return keys;
+        } catch (Exception e) {
+            throw new IdentityBrokerException("Could not obtain user public keys from github.", e);
+        }
+    }
 }
